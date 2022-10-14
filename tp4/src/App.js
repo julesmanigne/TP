dca1d27e159db0adca45f421b1d6ae3c25959f55
Jules Manigne-Malan <64989111+jmjmpx@users.noreply.github.com>
2022-10-14 10:20:22 +0200

    Create tp4 folder

---
+++ tp4/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <button type="button" class="btn btn-primary">Primary</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
