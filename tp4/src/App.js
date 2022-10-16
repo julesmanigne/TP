393985a7d41e88e4f3d18204cf266884e5473344
Jules Manigne-Malan <64989111+jmjmpx@users.noreply.github.com>
2022-10-16 16:09:23 +0200

    création des boutons de profils

--- tp4/src/App.js
+++ tp4/src/App.js
@@ -5,11 +5,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <button type="button" class="btn btn-primary">Primary</button>
-      </header>
-    </div>
+<div className="App">
+
+  <header className="App-header">
+    <nav class="navbar navbar-light">
+      <form class="container-fluid justify-content-end">
+        <button class="btn btn-success me-4 justify-content-md-end" type="button">Jeanne</button>
+        <button class="btn btn-success me-4" type="button">Martine</button>
+        <button class="btn btn-success me-4" type="button">Claude</button>
+      </form>
+    </nav>
+  </header>
+
+<div>
+<img src="" alt="" />
+
+</div>
+
+
+</div>
   );
 }
 
